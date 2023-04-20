16027a546a69577d7ee2c2672aaa059b60b51014
sammibick <sbickford99@gmail.com>
2023-04-20 11:22:31 -0500

    Class work
    Class work 4/18

---
+++ 19.1/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,16 @@
+// Create the addNumbers function here.
+function addNumbers9() {
+    var two=2;
+    var three=3;
+
+    var five = two+three;
+    console.log(five);
+}
+addNumbers()
+// Create the stringConcat function here.
+function newString() {
+    var taco = 'taco'
+    var cat = 'cat'
+
+    var tacoCat = taco +''+cat;
+}
